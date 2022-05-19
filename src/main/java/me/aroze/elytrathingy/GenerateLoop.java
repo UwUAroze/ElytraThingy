3bb8a6628debf8658827264d0014fdcb615b0213
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-19 19:15:33 +0100

    a

--- src/main/java/me/aroze/elytrathingy/GenerateLoop.java
+++ src/main/java/me/aroze/elytrathingy/GenerateLoop.java
@@ -4,26 +4,29 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitTask;
 
-public class GenerateLoop implements Runnable {
+public class GenerateLoop{
+    public static void start() {
 
-    @Override
-    public void run() {
-        for (Player p : Bukkit.getOnlinePlayers()) {
-            if (p.isGliding()) {
+        BukkitTask task = Bukkit.getScheduler().runTaskTimer(ElytraThingy.getInstance(), () -> {
+            Bukkit.broadcastMessage("a");
+            for (Player p : Bukkit.getOnlinePlayers()) {
+                if (p.isGliding()) {
 
-                Location playerLoc = p.getLocation();
-                int addX = (int) (10 + (Math.random() * 30));
-                int addZ = (int) (10 + (Math.random() * 30));
-                Location hooploc = playerLoc.add(addX, 0, addZ);
+                    Location playerLoc = p.getLocation();
+                    int addX = (int) (10 + (Math.random() * 30));
+                    int addZ = (int) (10 + (Math.random() * 30));
+                    Location hooploc = playerLoc.add(addX, 0, addZ);
 
-                Bukkit.getWorld(p.getWorld().getName()).getBlockAt(hooploc.add(0, 1, 0)).setType(Material.PINK_WOOL);
-                Bukkit.getWorld(p.getWorld().getName()).getBlockAt(hooploc.add(0, -1, 0)).setType(Material.PINK_WOOL);
-                Bukkit.getWorld(p.getWorld().getName()).getBlockAt(hooploc.add(0, 0, 1)).setType(Material.PINK_WOOL);
-                Bukkit.getWorld(p.getWorld().getName()).getBlockAt(hooploc.add(0, 0, -1)).setType(Material.PINK_WOOL);
+                    Bukkit.getWorld(p.getWorld().getName()).getBlockAt(hooploc.add(0, 1, 0)).setType(Material.PINK_WOOL);
+                    Bukkit.getWorld(p.getWorld().getName()).getBlockAt(hooploc.add(0, -1, 0)).setType(Material.PINK_WOOL);
+                    Bukkit.getWorld(p.getWorld().getName()).getBlockAt(hooploc.add(0, 0, 1)).setType(Material.PINK_WOOL);
+                    Bukkit.getWorld(p.getWorld().getName()).getBlockAt(hooploc.add(0, 0, -1)).setType(Material.PINK_WOOL);
 
+                }
             }
-            }
+        } , 0, 200);
     }
 }
 
