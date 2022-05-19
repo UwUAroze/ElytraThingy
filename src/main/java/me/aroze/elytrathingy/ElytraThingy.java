6c3e7d4997394a43b1c1f9160811ff3ce1c94298
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-19 02:16:30 +0100

    Initial commit

---
+++ src/main/java/me/aroze/elytrathingy/ElytraThingy.java
@@ -0,0 +1,17 @@
+package me.aroze.elytrathingy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ElytraThingy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
