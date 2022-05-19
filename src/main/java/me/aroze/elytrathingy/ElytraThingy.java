e980bfe8c257387de42c9a66087a5e019756a515
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-19 19:15:30 +0100

    a

--- src/main/java/me/aroze/elytrathingy/ElytraThingy.java
+++ src/main/java/me/aroze/elytrathingy/ElytraThingy.java
@@ -5,13 +5,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
 public final class ElytraThingy extends JavaPlugin {
-    private int taskID = -1;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        BukkitTask task = Bukkit.getScheduler().runTaskTimer(this, new GenerateLoop(), 0L, 200L);
-        taskID = task.getTaskId();
+        GenerateLoop.start();
 
     }
 
@@ -19,7 +17,6 @@ public final class ElytraThingy extends JavaPlugin {
         @Override
         public void onDisable() {
             // Plugin shutdown logic
-            Bukkit.getScheduler().cancelTask(taskID);
         }
 
         public static ElytraThingy getInstance () {
