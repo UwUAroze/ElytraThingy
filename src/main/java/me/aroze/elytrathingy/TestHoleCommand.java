bc48fff117eaa413b47974a9a485e32087f5ef03
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-19 20:13:40 +0100

    Added left/right columns and comments to make things clearer

--- src/main/java/me/aroze/elytrathingy/TestHoleCommand.java
+++ src/main/java/me/aroze/elytrathingy/TestHoleCommand.java
@@ -19,11 +19,28 @@ public class TestHoleCommand implements CommandExecutor {
         Location bottomLeft = middle.clone().add(-2, -2, 0);
 
 
+        //Bottom row block catchers
         bottomLeft.clone().add(0, -1, 1).getBlock().setType(Material.BARRIER);
+        bottomLeft.clone().add(0, -1, 0).getBlock().setType(Material.BARRIER);
         bottomLeft.clone().add(0, -1, -1).getBlock().setType(Material.BARRIER);
+        //Column block catchers
+        bottomLeft.clone().add(0, -2, -2).getBlock().setType(Material.BARRIER);
+        bottomLeft.clone().add(0, -2, 2).getBlock().setType(Material.BARRIER);
 
+        //Bottom row blocks
         Bukkit.getWorld(((Player) sender).getWorld().getName()).spawnFallingBlock(bottomLeft.clone().add(0,10,1), Material.WHITE_CONCRETE, (byte) 0);
+        Bukkit.getWorld(((Player) sender).getWorld().getName()).spawnFallingBlock(bottomLeft.clone().add(0,10,0), Material.WHITE_CONCRETE, (byte) 0);
         Bukkit.getWorld(((Player) sender).getWorld().getName()).spawnFallingBlock(bottomLeft.clone().add(0,10,-1), Material.WHITE_CONCRETE, (byte) 0);
+
+        //Left column blocks
+        Bukkit.getWorld(((Player) sender).getWorld().getName()).spawnFallingBlock(bottomLeft.clone().add(0,10,-2), Material.WHITE_CONCRETE, (byte) 0);
+        Bukkit.getWorld(((Player) sender).getWorld().getName()).spawnFallingBlock(bottomLeft.clone().add(0,10,-2), Material.WHITE_CONCRETE, (byte) 0);
+        Bukkit.getWorld(((Player) sender).getWorld().getName()).spawnFallingBlock(bottomLeft.clone().add(0,10,-2), Material.WHITE_CONCRETE, (byte) 0);
+
+        //Right column blocks
+        Bukkit.getWorld(((Player) sender).getWorld().getName()).spawnFallingBlock(bottomLeft.clone().add(0,10,2), Material.WHITE_CONCRETE, (byte) 0);
+        Bukkit.getWorld(((Player) sender).getWorld().getName()).spawnFallingBlock(bottomLeft.clone().add(0,10,2), Material.WHITE_CONCRETE, (byte) 0);
+        Bukkit.getWorld(((Player) sender).getWorld().getName()).spawnFallingBlock(bottomLeft.clone().add(0,10,2), Material.WHITE_CONCRETE, (byte) 0);
 
 
         return true;
